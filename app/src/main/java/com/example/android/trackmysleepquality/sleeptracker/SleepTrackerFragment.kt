5b62531c7a41cec3602533c632edd04b1cdb1fd7
小俣幸之助 <rskowgshakhtardonetsk32517@gmail.com>
2019-12-18 14:40:41 +0900

    fix span size for header

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -20,7 +20,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -68,6 +67,12 @@ class SleepTrackerFragment : Fragment() {
 
         // add GridLayout
         val manager = GridLayoutManager(activity, 3)
+        manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int) = when (position) {
+                0 -> 3
+                else -> 1
+            }
+        }
         binding.sleepList.layoutManager = manager
 
         val adapter = SleepNightAdapter(SleepNightListener { nightId ->
@@ -83,7 +88,9 @@ class SleepTrackerFragment : Fragment() {
         viewModel.navigateToSleepDataQuality.observe(viewLifecycleOwner, Observer {
             it?.let {
                 findNavController().navigate(
-                    SleepTrackerFragmentDirections.actionSleepTrackerFragmentToSleepDetailFragment(it)
+                    SleepTrackerFragmentDirections.actionSleepTrackerFragmentToSleepDetailFragment(
+                        it
+                    )
                 )
                 viewModel.onSleepDataQualityNavigated()
             }
